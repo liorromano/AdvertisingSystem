662ba1aef9ea972e0af86fa08b4f4362eed631ad
lior romano <liorromano90@gmail.com>
2018-07-21 10:35:43 +0300

    third commit

---
+++ app/controllers/home/loginController.js
@@ -0,0 +1,34 @@
+/**
+ *Login controller
+ * verify admin log in - if the admin logged in he can manage the page
+ */
+
+home.controller('loginController', function ($scope, $routeParams, $http,$window) {
+
+
+    $scope.user=
+    {
+        "username": "",
+        "pass": ""
+    };
+
+    /**
+     * sign in function - verifies if the user and password are correct.
+     */
+    $scope.signIn = function() {
+        $http.post('verifyUser', $scope.user).
+            success(function(data, status) {
+                if(data.toString()=="true") {
+                    $window.location.href = '/list';
+                }
+                else {
+                    $window.alert("No such user! Please enter username and password again.");
+                }
+
+
+            })
+    };
+
+
+
+});
